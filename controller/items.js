67c6ff3e055ed7ce3f16abd9aba5b218ae3eb06c
zellcorp <zellcorp@zelltest.com>
2018-09-20 17:46:12 +0200

    evo1

---
+++ controller/items.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var router = express.Router();
+const Items = require('warframe-items');
+
+// define the about route
+router.get('/:category', function(req, res) {  
+  const items = new Items({
+    category:[req.params.category]
+  }, )
+  res.send(items);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
 var warframes = require('./controller/warframes');
 var images = require('./controller/images');
 var primaries = require('./controller/primaries');
 var secondaries = require('./controller/secondaries');
+var items = require('./controller/items');
 //var melee = require('./controller/melee');
 //var relics = require('./controller/relics');
 
+app.use(cors());
+app.use('/', items);
 app.use('/images', images);
 app.use('/warframes', warframes);
 app.use('/primaries', primaries);
@@ -18,4 +21,5 @@ app.use('/secondaries', secondaries);
 app.listen(3000, function () {
   console.log('server up')
 })
+
 
